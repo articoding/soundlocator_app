7862967efee7d9e66f6078ae7ad6c54855a0ef96
HornyFurry13 <betosamakiller8@gmail.com>
2024-04-09 09:22:33 -0500

    ahora si funcionan los metodos, o deberian de funcionar xd
    ai

---
+++ soundapp/www/Controllers/routes.js
@@ -0,0 +1,15 @@
+//Declare environment
+const local_server = "http://localhost:44397/api/"                         //Write localhost and port
+const public_server = ""                        //Write WEB API public address
+const local_sources = ""       //Write App local resources
+
+const env = local_server                                  //Select your environment (local or public server)
+
+//Users API
+
+const allUsers_route = env + "Users"
+const postUser_route = env + "Users/"
+const loginUser_route = env + "users/login"
+const getUser_route = env + "Users/"
+const totalUser_route = env + "Users/GetTotalUsers"
+const createUser = env + "Users"
